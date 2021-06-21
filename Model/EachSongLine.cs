e0a4167d36d09a9e83b50d4a4cc3d36405751ebc
gargiscode <85780951+gargiscode@users.noreply.github.com>
2021-06-21 13:42:05 -0700

    Add files via upload

---
+++ Model/EachSongLine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibrary.Model
+{
+    public class EachSongLine
+    {
+        public string SongTitle { get; set; }
+        public string AudioFile { get; set; }
+       
+    }
+}
